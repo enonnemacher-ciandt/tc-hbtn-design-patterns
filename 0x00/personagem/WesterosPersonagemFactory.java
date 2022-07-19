6c9ad027d32537ac3804f39259722f231697ab03
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-07-19 14:40:58 -0300

    Task 0-Factory Method de Personagens

---
+++ 0x00/personagem/WesterosPersonagemFactory.java
@@ -0,0 +1,15 @@
+public class WesterosPersonagemFactory extends PersonagemFactory {
+
+    @Override
+    public Personagem createPersonagem(TipoPersonagem tipo, String nome) throws Exception {
+        if (tipo.equals(TipoPersonagem.MAGO)) {
+            return new Mago(nome, 8, 3, 3, 3, 4);
+        } else if (tipo.equals(TipoPersonagem.LADRAO)) {
+            return new Ladrao(nome, 1, 8, 7, 7, 10);
+        } else if (tipo.equals(TipoPersonagem.GUERREIRO)) {
+            return new Guerreiro(nome, 0, 9, 7, 10, 7);
+        }
+
+        return null;
+    }
+}
