8c801cf01548fd6e50d5bd7209c93011deb0fa68
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-07-20 17:58:45 -0300

    Task 0-Servico Lanchonete Pedido Builder

---
+++ 0x02/lanchonete/Pedido.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+
+public class Pedido {
+
+    private HashSet<ItemPedido> itensDentroCaixa = new HashSet<>();
+    private HashSet<ItemPedido> itensForaCaixa = new HashSet<>();
+
+    public void adicionarItemDentroCaixa(ItemPedido item) {
+        itensDentroCaixa.add(item);
+    }
+
+    public void adicionarItemForaCaixa(ItemPedido item) {
+        itensForaCaixa.add(item);
+    }
+
+    @Override
+    public String toString() {
+        String pedido = "\tFora da Caixa:\n";
+        for (ItemPedido itemPedido : itensForaCaixa) {
+            pedido += (String.format("\t\t- %s %s \n", itemPedido.getTipo(), itemPedido.getNome()));
+        }
+
+        pedido += "\tDentro da Caixa:\n";
+        for (ItemPedido itemPedido : itensDentroCaixa) {
+            pedido += (String.format("\t\t- %s %s \n", itemPedido.getTipo(), itemPedido.getNome()));
+        }
+
+        return pedido;
+    }
+}
